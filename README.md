e4a4e5cd263828d71a263cd9166a517a5146ed01
MJ Brechner <mjbrechner@gmail.com>
2024-04-21 08:04:53 -0400

    buttons highlight names, styling updates, etc.

---
+++ README.md
@@ -0,0 +1,5 @@
+Presidential Guesser by M.J. Brechner
+
+This is an in-progress trvia game featuring the presidents of the United States of America.
+
+© 2024 M.J. Brechner
--- javascript/index.js
+++ javascript/index.js
@@ -17,7 +17,6 @@ function toggle() {
 }
 
 
-
 // Question Asking
 
 let questionBeingAsked;
@@ -54,3 +53,210 @@ async function getQuestions() {
 }
 
 getQuestions();
+
+
+
+// Question answering
+
+let chosenPresident;
+const chosenPresidentBox = document.getElementById("pres-name-box");
+
+
+function displayChosenPresident() {
+    document.getElementById("pres-button-area").style.pointerEvents = "none";
+    document.getElementById("pres-name-box").style.display = "none";
+}
+
+// When presidents are actually clicked and therefore chosen
+function chooserWashington() {
+    chosenPresident = "washington";
+    document.getElementById("answer-box").innerText = `You have chosen George Washington.`;
+    displayChosenPresident();
+
+}
+
+// Display president name when user hovers over their image without clicking to choose
+
+function hoverWashington() {
+    chosenPresidentBox.innerText = "George Washington";
+}
+
+function hoverAdams_J() {
+    chosenPresidentBox.innerText = "John Adams";
+}
+
+function hoverJefferson() {
+    chosenPresidentBox.innerText = "Thomas Jefferson";
+}
+
+function hoverMadison() {
+    chosenPresidentBox.innerText = "James Madison";
+}
+
+function hoverMonroe() {
+    chosenPresidentBox.innerText = "James Monroe";
+}
+
+function hoverAdams_JQ() {
+    chosenPresidentBox.innerText = "John Quincy Adams";
+}
+
+function hoverJackson() {
+    chosenPresidentBox.innerText = "Andrew Jackson";
+}
+
+function hoverVanBuren() {
+    chosenPresidentBox.innerText = "Martin Van Buren";
+}
+
+function hoverHarrison_WH() {
+    chosenPresidentBox.innerText = "William Henry Harrison";
+}
+
+function hoverTyler() {
+    chosenPresidentBox.innerText = "John Tyler";
+}
+
+function hoverPolk() {
+    chosenPresidentBox.innerText = "James Polk";
+}
+
+function hoverTaylor() {
+    chosenPresidentBox.innerText = "Zachary Taylor";
+}
+
+function hoverFillmore() {
+    chosenPresidentBox.innerText = "Millard Fillmore";
+}
+
+function hoverPierce() {
+    chosenPresidentBox.innerText = "Franklin Pierce";
+}
+
+function hoverBuchanan() {
+    chosenPresidentBox.innerText = "James Buchanan";
+}
+
+function hoverLincoln() {
+    chosenPresidentBox.innerText = "Abraham Lincoln";
+}
+
+function hoverJohnson_A() {
+    chosenPresidentBox.innerText = "Andrew Johnson";
+}
+
+function hoverGrant() {
+    chosenPresidentBox.innerText = "Ulysses S. Grant";
+}
+
+function hoverHayes() {
+    chosenPresidentBox.innerText = "Rutherford Hayes";
+}
+
+function hoverGarfield() {
+    chosenPresidentBox.innerText = "James Garfield";
+}
+
+function hoverArthur() {
+    chosenPresidentBox.innerText = "Chester Arthur";
+}
+
+function hoverCleveland() {
+    chosenPresidentBox.innerText = "Grover Cleveland";
+}
+
+function hoverHarrison_B() {
+    chosenPresidentBox.innerText = "Benjamin Harrison";
+}
+
+function hoverMcKinley() {
+    chosenPresidentBox.innerText = "William McKinley";
+}
+
+function hoverRoosevelt_T() {
+    chosenPresidentBox.innerText = "Teddy Roosevelt";
+}
+
+function hoverTaft() {
+    chosenPresidentBox.innerText = "William Howard Taft";
+}
+
+function hoverWilson() {
+    chosenPresidentBox.innerText = "Woodrow Wilson";
+}
+
+function hoverHarding() {
+    chosenPresidentBox.innerText = "Warren G. Harding";
+}
+
+function hoverCoolidge() {
+    chosenPresidentBox.innerText = "Calvin Coolidge";
+}
+
+function hoverHoover() {
+    chosenPresidentBox.innerText = "Herbert Hoover";
+}
+
+function hoverRoosevelt_FD() {
+    chosenPresidentBox.innerText = "Franklin D. Roosevelt";
+}
+
+function hoverTruman() {
+    chosenPresidentBox.innerText = "Harry Truman";
+}
+
+function hoverEisenhower() {
+    chosenPresidentBox.innerText = "Dwight Eisenhower";
+}
+
+function hoverKennedy() {
+    chosenPresidentBox.innerText = "John F. Kennedy";
+}
+
+function hoverJohnson_LB() {
+    chosenPresidentBox.innerText = "Lyndon Johnson";
+}
+
+function hoverNixon() {
+    chosenPresidentBox.innerText = "Richard Nixon";
+}
+
+function hoverFord() {
+    chosenPresidentBox.innerText = "Gerald Ford";
+}
+
+function hoverCarter() {
+    chosenPresidentBox.innerText = "Jimmy Carter";
+}
+
+function hoverReagan() {
+    chosenPresidentBox.innerText = "Ronald Reagan";
+}
+
+function hoverBush_GHW() {
+    chosenPresidentBox.innerText = "George H.W. Bush";
+}
+
+function hoverClinton() {
+    chosenPresidentBox.innerText = "Bill Clinton";
+}
+
+function hoverBush_GW() {
+    chosenPresidentBox.innerText = "George W. Bush";
+}
+
+function hoverObama() {
+    chosenPresidentBox.innerText = "Barack Obama";
+}
+
+function hoverTrump() {
+    chosenPresidentBox.innerText = "Donald Trump";
+}
+
+function hoverBiden() {
+    chosenPresidentBox.innerText = "Joe Biden";
+}
+
+function hoverNone() {
+    chosenPresidentBox.innerText = "";
+}
